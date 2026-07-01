301cbc5837e14b10b5999c76d133614fd8f6e7ef
kevinmau888 <kevinmau33@gmail.com>
2026-07-01 16:16:42 +0800

    fix1

--- src/index.js
+++ src/index.js
@@ -400,7 +400,7 @@ export default {
       return new Response(null, {
         headers: {
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'POST, OPTIONS',
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
           'Access-Control-Allow-Headers': 'Content-Type',
         },
       });
